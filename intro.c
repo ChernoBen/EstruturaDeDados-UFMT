63b5de578535cce3231f578fb5e832caa92293e3
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-17 16:26:49 -0300

    hello world

---
+++ intro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+"#include <stdio.h>" :
+permite que qualquer tipo de entrada ou saida
+seja processada pela linguagem
+*/
+
+int main(void) {
+  int integer;
+  float floatNumber;
+  
+  integer = 10;
+  floatNumber = 5.7;
+  
+  printf("Hello World\n");
+  return 0;
+}

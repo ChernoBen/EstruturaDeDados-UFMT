5767de373b9f2542534f5ac83fc8caaa207ec1e9
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-17 16:52:09 -0300

    feat: condicionais

--- intro.c
+++ intro.c
@@ -13,6 +13,28 @@ int main(void) {
   integer = 10;
   floatNumber = 5.7;
   
+  //conditionals
+  if(integer>floatNumber){
+    printf("int is bigger than float\n");
+  }else{
+    printf("float is bigger than int\n");
+  }
+
+  //cases
+  switch(integer){
+    case 1:
+      printf("Value is 1\n");
+      break;
+    case 2:
+      printf("Value is 2\n");
+      break;
+    case 10:
+      printf("Value is 10\n");
+      break;
+    default:
+      printf("Unexpected value\n");
+  }
+  
   printf("Hello World\n");
   return 0;
 }

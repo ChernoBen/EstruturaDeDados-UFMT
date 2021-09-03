354a1ca1bab2a3f49899fbdfd1d5b5010c0786cc
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-09-03 07:57:02 -0400

    finalização at7

--- ativ7/main.c
+++ ativ7/main.c
@@ -12,26 +12,32 @@ int main(){
   tipo_lista lista;
   int entrada = 9;
   // 1# Criar um heap vazio
+  printf("Questão 1:\n");
   inicializaHeap(&heap);
   imprimeHeap(heap);
 
   // 2# Inserir um novo valor
+  printf("Questão 2:\n");
   insereHeap(&heap,5);
   insereHeap(&heap,4);
   insereHeap(&heap,2);
   imprimeHeap(heap);
 
   // 3# Consultar valor máximo da Raiz
+  printf("Questão 3:\n");
   printf("Valor maximo da raiz: %d\n",heap.heap[0]);
 
   // 4# Remover valor máximo do heap
+  printf("Questão 4:\n");
   printf("Valor máximo removido: %d\n",removeHeap(&heap));
   imprimeHeap(heap);
 
-  //Retornar quantidade de valores armazenados no heap
+  // 5# Retornar quantidade de valores armazenados no heap
+  printf("Questão 5:\n");
   printf("Qtd valores armazenado no heap: %d\n",heap.cont);
 
   // 6# Verificar se um dado valor existe dentro do heap
+  printf("Questão 6:\n");
   if(verificaHeap(heap,entrada)==1){
     printf("Valor %d existe dentro do heap\n",entrada);
   }else{
@@ -39,6 +45,7 @@ int main(){
   }
 
   // 7# Unificar dois Heaps em um unico;
+  printf("Questão 7:\n");
   insereHeap(&heap2,9);
   insereHeap(&heap2,1);
   insereHeap(&heap2,7);
@@ -46,6 +53,7 @@ int main(){
   imprimeHeap(heapConcatenado);
   
   // 8# Ordenar Heaps utilizadno heapSort
+  printf("Questão 8:\n");
   lista = migraHeap(heapConcatenado);
   imprimeLista(lista);
   heapSort(&lista);

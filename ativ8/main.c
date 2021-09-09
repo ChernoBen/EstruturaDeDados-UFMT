cad7cfd1272a5fdb41202607b7dff4779f9f9f17
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-09-09 17:15:11 -0400

    finalização l1

--- ativ8/main.c
+++ ativ8/main.c
@@ -5,9 +5,11 @@
 
 int main(){
     //1#
+    printf("QUESTAO 1#");
     tipo_arv_bin *arvore;
     arvore = NULL;
     //2#
+    printf("QUESTAO 2#");
     insereArvBin(&arvore,10);
     insereArvBin(&arvore,3);
     insereArvBin(&arvore,9);
@@ -18,6 +20,7 @@ int main(){
     insereArvBin(&arvore,9);
     insereArvBin(&arvore,14);
     //3#
+    printf("QUESTAO 3#");
     int res = buscaValor(arvore,11);
     if(res == (-1)){
         printf("Valor nao encontrado\n");   
@@ -25,24 +28,32 @@ int main(){
         printf("Resultado: %d\n",res);    
     }
     //4#
+    printf("QUESTAO 4#\n");
     arvore = removeValor(arvore,9);
     //5#
+    printf("QUESTAO 5#\n");
     printf("total: %d\n",contaBilizaBin(arvore));
     //6#
+    printf("QUESTAO 6#\n");
     printf("Altura: %d\n",obterAlturaArv(arvore));
     //7#
+    printf("QUESTAO 7#\n");
     imprimeFolhas(arvore);
     printf("\n\n");
     //8#
+    printf("QUESTAO 8#\n");
     imprimirNiveis(arvore);
     printf("\n\n");
     //9#
+    printf("QUESTAO 9#\n");
     percursoPreOrdem(arvore);
     printf("\n");
     //10#
+    printf("QUESTAO 10#\n");
     percursoPosOrdem(arvore);
     printf("\n");
     //11#
+    printf("QUESTAO 11#\n");
     percursoOrdem(arvore);
     printf("\n");
     return 1;

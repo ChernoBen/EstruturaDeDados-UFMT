ee52c8cd0e257b41ca7466b410c3401462f627d7
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-09-10 17:48:10 -0400

    listas

---
+++ ativ8/listas/arvore_mult.c
@@ -0,0 +1,168 @@
+/*
+Implementação Arvore Multiplos filhos
+*/
+
+#ifndef ARVORE_MULT
+#define ARVORE_MULT
+#include <stdlib.h>
+#include <stdio.h>
+
+#define T 3
+//estrutura da arvore
+struct est_arv_mf{
+    int valores[T];
+    struct est_arv_mf *filhos[T+1];
+    int contador;
+};
+typedef struct est_arv_mf tipo_arv_mf;
+
+//prototipos
+tipo_arv_mf *alocaNovoNoArvMf(int valor);
+void insereArvMf(tipo_arv_mf **arv,int vl);
+void percursoPreOrdemMf(tipo_arv_mf *arv);
+void percursoPosOrdemMf(tipo_arv_mf *arv);
+int buscaValorMf(tipo_arv_mf *arv,int vl);
+int contaBilizaMf(tipo_arv_mf *arv);
+void imprimeFolhasMf(tipo_arv_mf *arv);
+
+//funcoes
+
+//funcao que imprime folhas mf
+void imprimeFolhasMf(tipo_arv_mf *arv){
+    int i=0;
+    if(arv==NULL){
+        printf("");
+    }else{
+        if(arv->filhos != NULL){
+            for(i=0;i<arv->contador-1;i++){
+                imprimeFolhasMf(arv->filhos[i]);
+            }
+            
+        }else{
+            for(i=0;i<T;i++){
+                printf("%d ",arv->valores[i]);
+            }
+        }
+    }
+}
+
+//funcao que retorna total de itens na arv mf
+int contaBilizaMf(tipo_arv_mf *arv){
+    int total,i;
+    total = 0;
+    if(arv == NULL){
+        return 0;
+    }else{
+        for(i=0;i<T;i++){
+            total ++;
+        }
+        for(i=0;i<=arv->contador;i++){
+            total += contaBilizaMf(arv->filhos[i]);
+        }
+        return total;
+    }
+}
+//aloca novo no
+tipo_arv_mf *alocaNovoNoArvMf(int valor){
+    int i;
+    tipo_arv_mf *novo_no;
+    novo_no = (tipo_arv_mf*) malloc(sizeof(tipo_arv_mf));
+    novo_no->valores[0] = valor;
+    novo_no->contador=1;
+    for(i=0;i<T+1;i++){
+        novo_no->filhos[i] = NULL;
+    }
+    return novo_no;
+}
+
+//funcao que insere um novo valor na estrutura
+void insereArvMf(tipo_arv_mf **arv,int vl){
+    int i;
+    if((*arv)==NULL){
+        (*arv) = alocaNovoNoArvMf(vl);
+    }else{
+        //se a arvore menor que T, fazer inserção ordenada
+        if((*arv)->contador<T){
+            i = (*arv)->contador-1;
+            while((i>=0)&&((*arv)->valores[i]>vl)){
+                (*arv)->valores[i+1] = (*arv)->valores[i];
+                i--;    
+            }
+            (*arv)->valores[i+1]=vl;
+            (*arv)->contador++;
+        }else{
+            //se no cheio, identifique filho correto p/inserção
+            i = 0;
+            //Enquanto i menor que T e valores no nó maior que vl, caminhe p/ frente
+            while(i<T && (*arv)->valores[i]<vl){
+                i++;
+            }
+            //faça a chamada recursiva na posição do filho identificado
+            insereArvMf(&(*arv)->filhos[i],vl);
+        }
+    }
+}
+
+//funcao que imprime em pre ordem arv MF
+void percursoPreOrdemMf(tipo_arv_mf *arv){
+    if(arv!=NULL){
+        int i;
+        for (i=0;i<arv->contador;i++){
+            printf("%d ", arv->valores[i]);
+        }
+        //verifica se no e completo e chama recursivamente p/ os filhos
+        if(arv->contador == T){
+            for(i=0;i<T+1;i++){
+                percursoPreOrdemMf(arv->filhos[i]);
+            }
+        }
+    }
+}
+
+//funcao que imprime em pos ordem arv MF
+void percursoPosOrdemMf(tipo_arv_mf *arv){
+    if(arv!=NULL){
+        int i;
+        if(arv->contador == T){
+            for(i=0;i<T+1;i++){
+                percursoPreOrdemMf(arv->filhos[i]);
+            }
+        }
+        for (i=0;i<arv->contador;i++){
+            printf("%d ", arv->valores[i]);
+        }
+    }
+}
+
+//função que busca valor dentro da arvore mf
+int buscaValorMf(tipo_arv_mf *arv,int vl){
+    if(arv !=NULL){
+        int i = 0;
+        while(i<arv->contador && vl > arv->valores[i]){
+            i++;
+        }
+        if(i<arv->contador && vl == arv->valores[i]){
+            return vl;
+        }else if(arv->filhos[i]!=NULL){
+            return buscaValorMf(arv->filhos[i],vl);
+        }else{
+            return -1;
+        }
+    }else{
+        return -1;
+    }
+}
+
+//Em Ordem
+void imprimirOrdemMf(tipo_arv_mf *arv){
+    if(arv==NULL){
+        return;
+    }
+    for(int i =0;i<arv->contador;i++){
+        imprimirOrdemMf(arv->filhos[i]);
+        printf("%d ",arv->valores[i]);
+    }
+    imprimirOrdemMf(arv->filhos[arv->contador]);
+}
+
+#endif // !ARVORE_MULT
---
+++ ativ8/listas/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arvore_binaria.c"
+#include "arvore_mult.c"
+
+int main(){
+    /*
+    Lista 1
+    */
+    //1#
+    printf("QUESTAO 1#\n");
+    tipo_arv_bin *arvore;
+    arvore = NULL;
+    //2#
+    printf("QUESTAO 2#\n");
+    insereArvBin(&arvore,10);
+    insereArvBin(&arvore,3);
+    insereArvBin(&arvore,9);
+    insereArvBin(&arvore,13);
+    insereArvBin(&arvore,18);
+    insereArvBin(&arvore,20);
+    insereArvBin(&arvore,40);
+    insereArvBin(&arvore,9);
+    insereArvBin(&arvore,14);
+    //3#
+    printf("QUESTAO 3#\n");
+    int res = buscaValor(arvore,11);
+    if(res == (-1)){
+        printf("Valor nao encontrado\n");   
+    }else{
+        printf("Resultado: %d\n",res);    
+    }
+    //4#
+    printf("QUESTAO 4#\n");
+    arvore = removeValor(arvore,9);
+    //5#
+    printf("QUESTAO 5#\n");
+    printf("total: %d\n",contaBilizaBin(arvore));
+    //6#
+    printf("QUESTAO 6#\n");
+    printf("Altura: %d\n",obterAlturaArv(arvore));
+    //7#
+    printf("QUESTAO 7#\n");
+    imprimeFolhas(arvore);
+    printf("\n\n");
+    //8#
+    printf("QUESTAO 8#\n");
+    imprimirNiveis(arvore);
+    printf("\n\n");
+    //9#
+    printf("QUESTAO 9#\n");
+    percursoPreOrdem(arvore);
+    printf("\n");
+    //10#
+    printf("QUESTAO 10#\n");
+    percursoPosOrdem(arvore);
+    printf("\n");
+    //11#
+    printf("QUESTAO 11#\n");
+    percursoOrdem(arvore);
+    printf("\n");
+    /*
+    Lista 2
+    */
+    printf("Lista 2\n");
+    printf("1#\n");
+    //1#
+    tipo_arv_mf *arv;
+    arv = NULL;
+    //2#
+    printf("2# \n");
+    insereArvMf(&arv,10);
+    insereArvMf(&arv,7);
+    insereArvMf(&arv,1);
+    insereArvMf(&arv,15);
+    insereArvMf(&arv,0);
+    insereArvMf(&arv,5);
+    insereArvMf(&arv,8);
+    //insereArvMf(&arv,6);
+    //insereArvMf(&arv,2);
+    //3#
+    printf("3#\n");
+    res = buscaValorMf(arv,9);
+    if(res == (-1)){
+        printf("Valor não encontrado ou arvore é nula\n");
+    }else{
+        printf("Valor %d encontrado!\n",res);
+    }
+    //9#
+    printf("9#\n");
+    percursoPreOrdemMf(arv);
+    printf("\n");
+    //10#
+    printf("10#\n");
+    percursoPosOrdemMf(arv);
+    printf("\n");
+    //11#
+    printf("11#\n");
+    imprimirOrdemMf(arv);
+    printf("\n");
+    //insereArvMf(&arv,1);
+    //printf("%d | %d | %d | %d \n",arv->valores[0],arv->valores[1],arv->valores[2],arv->valores[3]);
+    return 1;
+}
